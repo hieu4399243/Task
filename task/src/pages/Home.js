f14bd8571868289bd01fcbd04ea10e6e31d347b0
HiYi <hieu243268@gmail.com>
2024-03-22 20:27:55 -0700

    create link router

--- task/src/pages/Home.js
+++ task/src/pages/Home.js
@@ -1,29 +1,33 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../App.css";
 
 export default function Home() {
-    const home = () => {
-        return (
-            <div>
-            <nav>
-            <ul>
-                <li>
-                <Link to="/chap1">Chap 1</Link>
-                </li>
-                <li>
-                <Link to="/about">About</Link>
-                </li>
-                <li>
-                <Link to="/users">Users</Link>
-                </li>
-            </ul>
-            </nav>
-            </div>
-        )
-    };
+  const home = () => {
     return (
-    <div>
-        {home()}
-    </div>
-  )
+      <div className="home">
+        <h1>List task</h1>
+        <nav>
+          <ul style={{padding:'0px'}}>
+            <li className="chap">
+              <Link to="/chap1">Chap Zero: Function component and Class component</Link>
+            </li>
+            <li className="chap">
+              <Link to="/chap2">Chap 1: Vòng đời của component</Link>
+            </li>
+            <li className="chap">
+              <Link to="/chap3">Chap 2: State and Props</Link>
+            </li>
+            <li className="chap">
+              <Link to="/chap3">Chap 3: Các thẻ HTML</Link>
+            </li>
+            <li className="chap">
+              <Link to="/chap3">Chap 4: Redux</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  };
+  return <div>{home()}</div>;
 }
