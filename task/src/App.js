794347bab641798fb3bddafbc2fd8cb34daaf125
HiYi <hieu243268@gmail.com>
2024-03-22 20:32:34 -0700

    change link

--- task/src/App.js
+++ task/src/App.js
@@ -6,13 +6,18 @@ import {
 
 } from "react-router-dom";
 import Home from './pages/Home';
+import Chap1 from './pages/chap1/chap1';
 
 function App() {
   return (
     <div>
     <Routes>
       <Route path="/" element={<Home/>}></Route>
-      
+      <Route path='/chap1' element={<Chap1/>}>
+      <Route path='/chap2' element={<Chap1/>}></Route>
+      <Route path='/chap3' element={<Chap1/>}></Route>
+      <Route path='/chap4' element={<Chap1/>}></Route>
+      </Route>
     </Routes>
   </div>
   );
